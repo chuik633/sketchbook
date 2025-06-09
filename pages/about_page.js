bbc0b3359ecf40e7820306d47eadacfb290ddf0e
Katherine Chui <chuik633@newschool.edu>
2025-06-09 12:56:56 -0400

    checkpoint

--- pages/about_page.js
+++ pages/about_page.js
@@ -2,6 +2,6 @@
 function layout_about_page(container){
     const page = container.append('div').attr('class', 'page about-page')
     page.append('h1').text("KATHERINE CHUI")
-    page.append('p').text("Welcome to my digital sketchbook/archive! I’m currently studying data visualization (MS) at Parsons. In my undergrad at Rice, I studied Math and Computer Science, but have always loved visual art and working across disciplines. Postgrad, I’ve found my niche in data viz and creative coding where I can experiment with code, design, and social issues. ")
+    page.append('p').text("Welcome to my digital sketchbook/archive! I’m currently studying data visualization (MS) at Parsons. In my undergrad at Rice, I studied Math and Computer Science, but have always loved visual art and working across disciplines. ")
     return page
 }
